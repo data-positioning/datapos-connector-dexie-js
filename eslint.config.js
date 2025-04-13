efc3dcf7293a94ea3982f05f0976afa3e4416202
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-04-13 21:32:26 +0100

    v0.2.58

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+// Dependencies - Vendor
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+// Exposures
+/** @type {import('eslint').Linter.Config[]} */
+export default [{ files: ['**/*.{js,mjs,cjs,ts}'] }, { languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended, ...tseslint.configs.recommended];
